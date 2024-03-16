8dfe9d6b7910ae3cbf0f5f7eb13f665373467ee2
fucherng1375 <fucherng1375@gmail.com>
2024-03-16 21:41:44 +0800

    Initial commite

---
+++ SwampOut/Source/SwampOut/SwampOut.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SwampOut.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SwampOut, "SwampOut" );
